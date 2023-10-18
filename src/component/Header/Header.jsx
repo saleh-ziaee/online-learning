a28854297cc68a2c70dd3a45def4bbff43b6c8c1
mahdi <109016246+mahdi81007@users.noreply.github.com>
2023-10-18 17:25:29 +0330

    nothing !

---
+++ src/component/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Header(props) {
+    return (
+        <div className={"flex"}>
+            <div className={""}>
+
+            </div>
+            <div>
+            </div>
+        </div>
+    );
+}
+
+export default Header;
