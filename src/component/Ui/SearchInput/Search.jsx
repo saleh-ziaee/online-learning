190c93e7ac0e6300d928f35294815be992b50f78
mahdi <109016246+mahdi81007@users.noreply.github.com>
2023-10-21 18:53:40 +0330

    fix search component

--- src/component/Ui/SearchInput/Search.jsx
+++ src/component/Ui/SearchInput/Search.jsx
@@ -1,18 +1,14 @@
 import  SearchIcon from "../../../assets/icon/SearchIcon.svg"
+import Button from "@/component/Ui/Button/Button.jsx";
 
 
 function search ({placeholder='جست و جو  '}){
   return (
-    <div className="inline-block ">
+    <div className="inline-block">
         <div dir="rtl" className="flex  justify-between px-5  items-center bg-[#FAFAFA] rounded-xl h-[64px] w-[252px]">
-            {
-                Button && Button
-            }
             <input maxLength="1000" className="bg-transparent focus-visible:outline-none h-[100%] flex-grow" placeholder={placeholder}/>
           <img src={SearchIcon}  className="flex h-5  "/>
-
         </div>
-
     </div>
   )
 }
