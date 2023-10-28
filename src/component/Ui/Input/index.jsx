8b81fa5028ed0db00213159cbb0f21f7c1ed9d8d
Ali Soltani <soltanipoor1@gmail.com>
2023-10-28 20:04:55 +0330

    auth feature part 1

--- src/component/Ui/Input/index.jsx
+++ src/component/Ui/Input/index.jsx
@@ -3,6 +3,7 @@ import { useId } from "react";
 function Input({
   type = "text",
   placeholder = "",
+  error,
   label,
   value,
   onChange,
@@ -27,13 +28,18 @@ function Input({
         value={value}
         onChange={onChange}
         placeholder={placeholder}
-        className={"bg-secondary-200 rounded-xl px-4 py-3 w-full"}
+        className={`bg-secondary-200 rounded-xl px-4 py-3 w-full border ${
+          error ? "border-red-500" : "border-transparent"
+        }`}
       />
+
+      {error && <div className="text-red-500 text-sm mt-2">{error}</div>}
     </div>
   );
 }
 
 Input.propTypes = {
+  error: PropTypes.string,
   name: PropTypes.string,
   value: PropTypes.string,
   onChange: PropTypes.func,
